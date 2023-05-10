14d92f722beb506e4b01f4b82631bf4ad9f6e263
“rene-weerts” <rjmwrt@gmail.com>
2023-05-10 20:50:36 +0200

    eerste setup van project

---
+++ src/main/java/com/eindopdracht/BE/ReneWeerts/PedicureAmbulantApplication.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.BE.ReneWeerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedicureAmbulantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedicureAmbulantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eindopdracht/BE/ReneWeerts/PedicureAmbulantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.BE.ReneWeerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedicureAmbulantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
